501bc04f11195bb06ad616abc87db83ebdb41786
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-05-23 08:23:29 +0300

    Modify GitGithub

--- com/gmail/dmytronechkaliuk/homeworks/homework03/GitGithub.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework03/GitGithub.java
@@ -2,6 +2,6 @@ package com.gmail.dmytronechkaliuk.homeworks.homework03;
 
 public class GitGithub {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }

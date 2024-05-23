6b189b1d6dd2183311f7bf83582aaea83d01f38c
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-05-23 08:18:59 +0300

    Initial commit. Create GitGithub

---
+++ com/gmail/dmytronechkaliuk/homeworks/homework03/GitGithub.java
@@ -0,0 +1,7 @@
+package com.gmail.dmytronechkaliuk.homeworks.homework03;
+
+public class GitGithub {
+    public static void main(String[] args) {
+        System.out.println("This is shop-online app");
+    }
+}

5db38bc3018bdad2434c6342c733e3209406f94c
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-05-25 09:13:00 +0300

    Update SalesAmount

--- com/gmail/dmytronechkaliuk/homeworks/homework04/SalesAmount.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework04/SalesAmount.java
@@ -1,2 +1,23 @@
-package com.gmail.dmytronechkaliuk.homeworks.homework04;public class SalesAmount {
+package com.gmail.dmytronechkaliuk.homeworks.homework04;
+
+public class SalesAmount {
+    public static void main(String[] args) {
+        int order = 1;
+        String product = "smartphone";
+        int days = 5;
+        double price = 12153.41;
+
+        System.out.printf("Product No %d: %s,\n", order, product);
+        System.out.printf("total sales for %d is EUR %.2f,\n", days, price);
+        System.out.printf("sales by day is EUR %.2f.\n", (price / days));
+
+        order = 2;
+        product = "laptop";
+        days = 7;
+        price = 10486.85;
+
+        System.out.printf("Product No %d: %s,\n", order, product);
+        System.out.printf("total sales for %d is EUR %.2f,\n", days, price);
+        System.out.printf("sales by day is EUR %.2f.\n", (price / days));
+    }
 }

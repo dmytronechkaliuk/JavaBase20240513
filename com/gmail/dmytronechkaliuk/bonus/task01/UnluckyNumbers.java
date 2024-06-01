f73fe86094934bc8a1397799e0b68b5d70cde0ba
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-01 11:31:14 +0300

    Update UnluckyNumbers

--- com/gmail/dmytronechkaliuk/bonus/task01/UnluckyNumbers.java
+++ com/gmail/dmytronechkaliuk/bonus/task01/UnluckyNumbers.java
@@ -2,6 +2,48 @@ package com.gmail.dmytronechkaliuk.bonus.task01;
 
 public class UnluckyNumbers {
     public static void main(String[] args) {
+        // System.out.println(hasFour(41));
+        // System.out.println(hasNine(39));
+
+        printShuttles();
 
     }
+
+    public static boolean hasFour(int number) {
+        while (number > 0) {
+            if (number % 10 == 4) {
+
+                return true;
+            }
+
+            number /= 10;
+        }
+
+        return  false;
+    }
+
+    public static boolean hasNine(int number) {
+        while (number > 0) {
+            if (number % 10 == 9) {
+
+                return true;
+            }
+
+            number /= 10;
+        }
+
+        return false;
+    }
+
+    public static void printShuttles() {
+        for (int i = 1; i <= 100; i ++) {
+            if (hasFour(i) || hasNine(i)) {
+                continue;
+            }
+
+            System.out.println("Space shuttle: " + i);
+        }
+
+    }
+
 }

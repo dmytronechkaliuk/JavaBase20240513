2d2f0afcaa939bfb3abd704b3d6b2857a70ed5f0
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-16 12:26:52 +0300

    Modify TicTacToe

--- com/gmail/dmytronechkaliuk/capstone/capstone02/TicTacToe.java
+++ com/gmail/dmytronechkaliuk/capstone/capstone02/TicTacToe.java
@@ -120,7 +120,6 @@ public class TicTacToe {
 
     public static int checkColumns(char[][] board) {
         int count = 0;
-
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[j][i] == 'X') {
@@ -142,7 +141,6 @@ public class TicTacToe {
 
     public static int checkLeft(char[][] board) {
         int count = 0;
-
         for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 'X') {
                 count++;
@@ -156,7 +154,6 @@ public class TicTacToe {
 
     public static int checkRight(char[][] board) {
         int count = 0;
-
         for (int i = 0; i < board.length; i++) {
             if (board[2 - i][i] == 'X') {
                 count++;

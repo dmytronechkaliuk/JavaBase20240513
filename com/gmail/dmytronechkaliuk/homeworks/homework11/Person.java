ec90901285b5f6dc582d4e0f868cdf538f83352f
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-22 14:51:12 +0300

    Create constructor. Add describe method

--- com/gmail/dmytronechkaliuk/homeworks/homework11/Person.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework11/Person.java
@@ -1,4 +1,21 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework11;
 
 public class Person {
+    String name;
+    int age;
+    String job;
+
+    public Person(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    public void describe() {
+        System.out.printf("Name: %s, Age: %d, Job: %s\n", name, age, job);
+    }
+
+    public void updateJob(String job) {
+        this.job = job;
+    }
 }

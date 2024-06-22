e66561cdf79fc7d990b75ee40ad86f59f27c1929
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-22 14:53:32 +0300

    Display Person objects. Update person job

--- com/gmail/dmytronechkaliuk/homeworks/homework11/PersonDescribe.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework11/PersonDescribe.java
@@ -2,6 +2,19 @@ package com.gmail.dmytronechkaliuk.homeworks.homework11;
 
 public class PersonDescribe {
     public static void main(String[] args) {
-        
+        Person firstPerson = new Person("John", 30, "Engineer");
+        Person secondPerson = new Person("Mary", 25, "Teacher");
+        Person thirdPerson = new Person("Bob", 35, "Doctor");
+
+        firstPerson.describe();
+        secondPerson.describe();
+        thirdPerson.describe();
+
+        Person fourthPerson = new Person("Alice", 28, "Architect");
+        fourthPerson.describe();
+
+        System.out.println("After updating the profession: ");
+        fourthPerson.updateJob("Designer");
+        fourthPerson.describe();
     }
 }

941a5148519ad02320ced2fda6db404155a6b04c
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-27 21:23:32 +0300

    Update CarRunner

--- com/gmail/dmytronechkaliuk/homeworks/homework12/CarRunner.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework12/CarRunner.java
@@ -1,4 +1,9 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework12;
 
 public class CarRunner {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.start();
+    }
 }

c63e0fe4a30484c449cb8d0136fb368c23296df2
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-27 21:22:43 +0300

    Update Car

--- com/gmail/dmytronechkaliuk/homeworks/homework12/Car.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework12/Car.java
@@ -1,4 +1,28 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework12;
 
 public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+        System.out.println("Car has started");
+    }
+
+    public void stop() {
+        System.out.println("Car has stopped");
+    }
+
+    private void startElectricity() {
+        System.out.println("Electricity started...");
+    }
+
+    private void startCommand() {
+        System.out.println("Command started...");
+    }
+
+    private void startFuelSystem() {
+        System.out.println("Fuel system started...");
+    }
+
 }

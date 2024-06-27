691040d630c5082371ec92c57596a9abca6b5e8e
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-27 13:59:27 +0300

    Update Employee

--- com/gmail/dmytronechkaliuk/homeworks/homework12/Employee.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework12/Employee.java
@@ -1,4 +1,59 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework12;
 
 public class Employee {
+
+    private String name;
+    private String job;
+    private String email;
+    private String phone;
+    private int age;
+
+    public Employee(String name, String job, String email, String phone, int age) {
+        this.name = name;
+        this.job = job;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
---
+++ com/gmail/dmytronechkaliuk/homeworks/homework12/EmployeeDescribe.java
@@ -0,0 +1,2 @@
+package com.gmail.dmytronechkaliuk.homeworks.homework12;public class EmployeeDescribe {
+}

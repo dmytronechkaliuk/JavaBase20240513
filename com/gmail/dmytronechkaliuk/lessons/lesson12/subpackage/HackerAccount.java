3888d9b54999c88d7caf7f8eadda05093ac4ca87
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-27 09:19:07 +0300

    Create HackerAccount

--- com/gmail/dmytronechkaliuk/lessons/lesson12/subpackage/HackerAccount.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson12/subpackage/HackerAccount.java
@@ -1,2 +1,18 @@
-package com.gmail.dmytronechkaliuk.lessons.lesson12.subpackage;public class HackerAccount {
+package com.gmail.dmytronechkaliuk.lessons.lesson12.subpackage;
+
+import com.gmail.dmytronechkaliuk.lessons.lesson12.BankAccount;
+
+public class HackerAccount {
+
+    private BankAccount account;
+
+    public HackerAccount(BankAccount account) {
+        this.account = account;
+    }
+
+    public void hack() {
+        int money = account.getMoney();
+        account.setMoney(-money);
+    }
+
 }

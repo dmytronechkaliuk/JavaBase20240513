ce7e735bc7c25ef931c0bb45a32d52cb62628307
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-27 15:55:35 +0300

    Update WrapperDemo

--- com/gmail/dmytronechkaliuk/lessons/lesson11/WrapperDemo.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson11/WrapperDemo.java
@@ -2,6 +2,17 @@ package com.gmail.dmytronechkaliuk.lessons.lesson11;
 
 public class WrapperDemo {
     public static void main(String[] args) {
-        
+        int value = 1;
+        Integer a = new Integer(1);
+        Integer b = new Integer(42);
+        // Integer b = null;
+        // int result = a.intValue() + b.intValue();
+        Integer result = a + b;
+
+        System.out.println(result);
+
+        System.out.println(Integer.toBinaryString(value));
+        System.out.println("int min value = " + Integer.MIN_VALUE);
+        System.out.println("int max value = " + Integer.MAX_VALUE);
     }
 }

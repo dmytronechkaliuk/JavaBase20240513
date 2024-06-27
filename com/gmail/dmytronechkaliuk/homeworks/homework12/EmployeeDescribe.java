0ab1e8bcf77e38990bc8d4d98c684041ee290dd4
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-27 14:02:45 +0300

    Update EmployeeDescribe

--- com/gmail/dmytronechkaliuk/homeworks/homework12/EmployeeDescribe.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework12/EmployeeDescribe.java
@@ -2,6 +2,8 @@ package com.gmail.dmytronechkaliuk.homeworks.homework12;
 
 public class EmployeeDescribe {
     public static void main(String[] args) {
+        Employee employee = new Employee("Dmytro Nechkaliuk", "Backend Developer", "0949321107d@gmail.com", "+380949321107", 18);
 
+        System.out.printf("Name: %s, Job: %s, Email: %s, Phone: %s, Age: %d", employee.getName(), employee.getJob(), employee.getEmail(), employee.getPhone(), employee.getAge());
     }
 }

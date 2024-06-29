adcf6cce917e94827d380d2c7fca38339a22adb5
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-06-29 21:18:59 +0300

    Fix EmployeeDescribe. Add toString() method

--- com/gmail/dmytronechkaliuk/homeworks/homework12/EmployeeDescribe.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework12/EmployeeDescribe.java
@@ -4,6 +4,6 @@ public class EmployeeDescribe {
     public static void main(String[] args) {
         Employee employee = new Employee("Dmytro Nechkaliuk", "Backend Developer", "0949321107d@gmail.com", "+380949321107", 18);
 
-        System.out.printf("Name: %s, Job: %s, Email: %s, Phone: %s, Age: %d", employee.getName(), employee.getJob(), employee.getEmail(), employee.getPhone(), employee.getAge());
+        System.out.println(employee);
     }
 }

c0644529d4a48254ed12c150c09fc31bcf5fa920
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-03 12:06:12 +0300

    Update AnimalRunner

--- com/gmail/dmytronechkaliuk/lessons/lesson12/AnimalRunner.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson12/AnimalRunner.java
@@ -1,4 +1,23 @@
 package com.gmail.dmytronechkaliuk.lessons.lesson12;
 
+import com.gmail.dmytronechkaliuk.lessons.lesson12.subpackage.Cat;
+
 public class AnimalRunner {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Garfield", 1, "black");
+        System.out.printf("A(an) %s cat %s says: \n", cat.getColor(), cat.getName());
+        cat.say();
+
+        System.out.println("toString() call: ");
+        System.out.println(cat);
+        System.out.println("info() call: ");
+        cat.info();
+
+        Animal animal = cat;
+        animal.say();
+
+        Cat clonedCat = new Cat("Garfield", 1, "black");
+        System.out.println("cat is cloned: " + (cat == clonedCat));
+        System.out.println("cat equals cloned: " + (cat.equals(clonedCat)));
+    }
 }

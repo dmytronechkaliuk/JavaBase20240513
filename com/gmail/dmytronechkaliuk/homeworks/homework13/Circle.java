a346c5e0193444b45dc56b426afdecc55d02c491
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 10:22:40 +0300

    Update Circle

--- com/gmail/dmytronechkaliuk/homeworks/homework13/Circle.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework13/Circle.java
@@ -1,4 +1,15 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework13;
 
-public class Circle {
+public class Circle implements GeometricShape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
 }

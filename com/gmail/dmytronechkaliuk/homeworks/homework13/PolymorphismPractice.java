50392771912f4ed883310cb14fa9490a453a0b42
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 10:21:46 +0300

    Update PolymorphismPractice

--- com/gmail/dmytronechkaliuk/homeworks/homework13/PolymorphismPractice.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework13/PolymorphismPractice.java
@@ -2,6 +2,24 @@ package com.gmail.dmytronechkaliuk.homeworks.homework13;
 
 public class PolymorphismPractice {
     public static void main(String[] args) {
-        
+        GeometricShape[] shapes = {
+                new Circle(3),
+                new Triangle(3, 4),
+                new Square(5)
+        };
+
+        double totalArea = calculateTotalArea(shapes);
+        System.out.println("The total area of all shapes: " + totalArea);
     }
+
+    public static double calculateTotalArea(GeometricShape[] shapes) {
+        double totalArea = 0;
+
+        for (GeometricShape shape : shapes) {
+            totalArea += shape.getArea();
+        }
+
+        return totalArea;
+    }
+
 }

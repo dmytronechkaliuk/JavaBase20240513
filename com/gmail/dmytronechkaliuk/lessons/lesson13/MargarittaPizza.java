c79352eb7e64fe0ecdcf901fe0760d4b5cf2b1bd
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 09:56:42 +0300

    Modify MargarittaPizza

--- com/gmail/dmytronechkaliuk/lessons/lesson13/MargarittaPizza.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson13/MargarittaPizza.java
@@ -1,4 +1,9 @@
 package com.gmail.dmytronechkaliuk.lessons.lesson13;
 
-public class MargarittaPizza {
+public class MargarittaPizza extends Pizza {
+
+    public MargarittaPizza() {
+        super("Margaritta", new String[] {"tomato", "cheese"});
+    }
+
 }

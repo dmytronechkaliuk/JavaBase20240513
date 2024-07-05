6b61d19a95a7bc91dc791ebf102f9b167dee2bc4
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 10:32:03 +0300

    Update Triangle

--- com/gmail/dmytronechkaliuk/homeworks/homework13/Triangle.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework13/Triangle.java
@@ -1,4 +1,17 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework13;
 
-public class Triangle {
+public class Triangle implements GeometricShape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * base * height;
+    }
+
 }

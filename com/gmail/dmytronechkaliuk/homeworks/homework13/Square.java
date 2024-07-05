171841fe7fce58e74690a858fa31ccea65089f65
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 10:32:33 +0300

    Update Square

--- com/gmail/dmytronechkaliuk/homeworks/homework13/Square.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework13/Square.java
@@ -1,4 +1,15 @@
 package com.gmail.dmytronechkaliuk.homeworks.homework13;
 
-public class Square {
+public class Square implements GeometricShape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
 }

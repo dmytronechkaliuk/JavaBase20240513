ebeb9588736149b29aa2886535137a3bf16da49f
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 09:54:59 +0300

    Modify Pizza

--- com/gmail/dmytronechkaliuk/lessons/lesson13/Pizza.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson13/Pizza.java
@@ -1,4 +1,28 @@
 package com.gmail.dmytronechkaliuk.lessons.lesson13;
 
-public class Pizza {
+import java.util.Arrays;
+
+public abstract class Pizza {
+    private String name;
+    private String[] ingredients;
+
+    public Pizza() {
+        System.out.println("Creating a default pizza...");
+    }
+
+    public Pizza(String name, String[] ingredients) {
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    public void make() {
+        System.out.println("Making a pizza: " + name);
+        System.out.println("Adding ingredients..." + Arrays.toString(ingredients));
+        addIngredients();
+    }
+
+    public void addIngredients() {
+
+    }
+
 }

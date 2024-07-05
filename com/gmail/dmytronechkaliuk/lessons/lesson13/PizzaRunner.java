5fd990ba4ca31b7dee84d87c730926cf37971758
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 09:55:24 +0300

    Modify PizzaRunner

--- com/gmail/dmytronechkaliuk/lessons/lesson13/PizzaRunner.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson13/PizzaRunner.java
@@ -3,6 +3,16 @@ package com.gmail.dmytronechkaliuk.lessons.lesson13;
 public class PizzaRunner {
 
     public static void main(String[] args) {
+        Pizza[] pizzas = {
+                new MargarittaPizza(),
+                new DiabloPizza()
+        };
 
+        for (Pizza pizza : pizzas) {
+            pizza.make();
+        }
+
+        // Pizza pizza = new Pizza();
     }
+
 }

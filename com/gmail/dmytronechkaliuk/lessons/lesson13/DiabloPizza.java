becc505de2096072de059b39a13c797a95e12c2f
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-05 09:55:56 +0300

    Modify DiabloPizza

--- com/gmail/dmytronechkaliuk/lessons/lesson13/DiabloPizza.java
+++ com/gmail/dmytronechkaliuk/lessons/lesson13/DiabloPizza.java
@@ -1,4 +1,9 @@
 package com.gmail.dmytronechkaliuk.lessons.lesson13;
 
-public class DiabloPizza {
+public class DiabloPizza extends Pizza {
+
+    public DiabloPizza() {
+        super("Diablo", new String[] {"chilly"});
+    }
+
 }

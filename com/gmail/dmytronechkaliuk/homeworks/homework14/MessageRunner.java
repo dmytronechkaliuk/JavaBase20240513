a3305e438e44daa808c01471ed505e09e8499525
Dmytro Nechkaliuk <0949321107d@gmail.com>
2024-07-08 20:19:13 +0300

    Update MessageRunner

--- com/gmail/dmytronechkaliuk/homeworks/homework14/MessageRunner.java
+++ com/gmail/dmytronechkaliuk/homeworks/homework14/MessageRunner.java
@@ -2,6 +2,15 @@ package com.gmail.dmytronechkaliuk.homeworks.homework14;
 
 public class MessageRunner {
     public static void main(String[] args) {
-        
+        MessagePrinter printer = new MessagePrinter();
+
+        MessagePrinter.Message firstMessage = new MessagePrinter.Message("Hello, World!", "Dmytro");
+        printer.print(firstMessage);
+
+        MessagePrinter.Message secondMessage = new MessagePrinter.Message("Hello, World!", null);
+        printer.print(secondMessage);
+
+        MessagePrinter.Message thirdMessage = new MessagePrinter.Message(null, null);
+        printer.print(thirdMessage);
     }
 }
